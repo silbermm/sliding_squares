a21fc90938c9d01d07f0a9e915f87be3267e2f38
Matt Silbernagel <silbermm@gmail.com>
2014-07-23 12:23:52 -0400

    refactoring to have a grid object

--- solver.js
+++ solver.js
@@ -5,6 +5,8 @@ var http_puzzle = require("./lib/http_puzzle.js");
 var puzzle = require("./lib/puzzle.js");
 var _ = require('lodash');
 var printf = require('printf');
+var Grid = require('./lib/tile.js').Grid;
+var width = 0;
 
 var cliData = cli.parseCli(process.argv);
 http_puzzle.fetchPuzzle(cliData.size, cliData.difficulty, function(response){     
@@ -14,18 +16,58 @@ http_puzzle.fetchPuzzle(cliData.size, cliData.difficulty, function(response){
   });
   response.on('end', function() {
     var resp = JSON.parse(res_data); 
-    
-    var preview = puzzle.buildPreview(resp.grid,resp.width);
-    console.log(preview);
-    
-    var grid = puzzle.build2dArray(resp.grid,resp.width);
- 
-    var coord = puzzle.findEmpty(grid);
-    console.log("empty is at: " + coord.row + ", " + coord.column);
-     
+   
+    var grid = new Grid(resp.grid, resp.width);
+    grid.setup;
   
+    console.log(grid.prettyPrint());
+ 
+    
+    /*
+    width = resp.width; 
+    var cells = puzzle.build2dArray(resp.grid,resp.width);
+    var neighbors = puzzle.findNeighbors(cells);
+   
+
+    var solution = []; 
+    var solutionTile = findSolution([cells], solution);
+ 
+    console.log("A solution was Found!");
+    console.log(solutionTile);
+    */
   });
 });
+
+
+
+/**
+ * Recursive function to find a solution!
+ * @param the current grid
+ * @param an array of Tiles that should be moved
+ */
+var findSolution = function(grids, solution){
+  var actualSolution = false;
+  var gridA = [];
+  _.each(grids,function(g){
+    if(puzzle.isSolution(_.flatten(g))){
+      actualSolution = true; 
+      console.log(puzzle.buildPreview(_.flatten(g),width));
+      return false;
+    } 
+    var neighbors = puzzle.findNeighbors(g);
+    _.each(neighbors, function(t){
+      var obj = puzzle.moveTile(g, t); 
+      gridA.push(obj.grid);
+      //console.log(obj.coordinate.moves);
+    });
+  }); 
+  if(actualSolution){
+    return solution;
+  }
+  return solution.push(findSolution(gridA, solution));
+}
+
+
 
 
 
--- spec/puzzle_spec.js
+++ spec/puzzle_spec.js
@@ -1,6 +1,7 @@
 var puzzle = require("../lib/puzzle.js");
 var printf = require('printf');
-var Coordinate = puzzle.Coordinate;
+var Tile = require("../lib/tile.js").Tile;
+var Grid = require("../lib/tile.js").Grid;
 
 describe("solution", function(){
   it("should be true", function(){
@@ -56,23 +57,66 @@ describe("Find empty", function(){
     expect(coordinates.row).toBe(1);
     expect(coordinates.column).toBe(2);
   });
+});
 
-  describe("Find Neighbors", function(){
-    it("should find 4",function(){
-      var twoD = [ [0,1,2],[3,null,4],[5,6,7] ];
-      var neighbors = puzzle.findNeighbors(twoD);
-      expect(neighbors.up.row).toBe(0);
-      expect(neighbors.down.row).toBe(2);
-    });
+describe("Find Neighbors", function(){
+  it("should find 4",function(){
+    var twoD = [ [0,1,2],[3,null,4],[5,6,7] ];
+    var neighbors = puzzle.findNeighbors(twoD);
+    expect(neighbors[0].row).toBe(0);
+    expect(neighbors[1].row).toBe(2);
+  });
+});
+
+describe("Is coordinate a neighbor", function(){
+  it("should be true for neighbors", function(){
+    var twoD = [ [0,1,2],[3,null,4],[5,6,7] ];
+    expect(puzzle.isNeighbor(twoD, new Tile(1,0))).toBe(true);
+    expect(puzzle.isNeighbor(twoD, new Tile(1,2))).toBe(true);
+    expect(puzzle.isNeighbor(twoD, new Tile(0,1))).toBe(true);
+    expect(puzzle.isNeighbor(twoD, new Tile(2,1))).toBe(true);
+    expect(puzzle.isNeighbor(twoD, new Tile(0,0))).toBe(false);
+    expect(puzzle.isNeighbor(twoD, new Tile(0,2))).toBe(false);
+    expect(puzzle.isNeighbor(twoD, new Tile(2,0))).toBe(false);
+    expect(puzzle.isNeighbor(twoD, new Tile(2,2))).toBe(false);
+  });
+});
+
+describe("Move Tile", function(){
+  it("should return a new grid with null moved", function(){
+    var twoD = [ [0,1,2],[3,null,4],[5,6,7] ];
+    
+    var coordinate = new Tile(1,0);
+    var newGridAndTile = puzzle.moveTile(twoD,coordinate);
+    
+    var newGrid = newGridAndTile.grid;
+    var newTile = newGridAndTile.coordinate;
+  
+    expect(newTile.row).toBe(1);
+    expect(newTile.column).toBe(1);
+
+    expect(newGrid[1][0]).toBe(null);
+
+  });
+});
+
+describe("Grid", function(){
+  it("should pretty print", function(){
+    var s = [0,1,2,3,4,5,6,7,null]
+    var pretty = '    0 |    1 |    2\r\n    3 |    4 |    5\r\n    6 |    7 | null\r\n'
+    var grid = new Grid(s,3);
+    grid.setup();
+    expect(grid.prettyPrint()).toBe(pretty);
   });
 
-  describe("is neighbor", function(){
-    it("should be", function(){
-      var twoD = [ [0,1,2],[3,null,4],[5,6,7] ];
-      expect(puzzle.isNeighbor(twoD, new Coordinate(1,0))).toBe(true);
-    });
-  });
+  it("should generate grid", function(){
+    var s = [0,1,2,3,4,5,6,7,null];
+    var grid = new Grid(s,3);
+    grid.setup();
+    var twoD = [ [0,1,2],[3,4,5],[6,7,null] ];
+    expect(grid.grid).toEqual(twoD);
+ });
 
-
+  
 });
 
